51702dd03ca209d2b6b83a25baba794e929e7eb5
Lucas Ladeira <lucas-ladeira@hotmail.com>
2022-06-15 09:43:25 -0300

    Customer orm map

--- src/main/kotlin/br/com/lucasladeira/mercadolivro/entities/Customer.kt
+++ src/main/kotlin/br/com/lucasladeira/mercadolivro/entities/Customer.kt
@@ -1,6 +1,12 @@
 package br.com.lucasladeira.mercadolivro.entities
 
+import javax.persistence.*
+
+@Entity
+@Table(name = "tb_customer")
 data class Customer(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long,
     var name: String,
     var email: String
