b7b7768d0eb63e59008fbdda665bc49ec1d9ad60
Lucas Ladeira <lucas-ladeira@hotmail.com>
2022-06-15 09:54:55 -0300

    Customer getAll and getById impl & endpoints

--- src/main/kotlin/br/com/lucasladeira/mercadolivro/controllers/CustomerController.kt
+++ src/main/kotlin/br/com/lucasladeira/mercadolivro/controllers/CustomerController.kt
@@ -4,10 +4,7 @@ import br.com.lucasladeira.mercadolivro.entities.Customer
 import br.com.lucasladeira.mercadolivro.services.CustomerService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/customers")
@@ -18,4 +15,13 @@ class CustomerController(var customerService: CustomerService) {
         return ResponseEntity.status(HttpStatus.CREATED).body(customerService.save(customer))
     }
 
+    @GetMapping
+    fun getAll(): ResponseEntity<List<Customer>>{
+        return ResponseEntity.status(HttpStatus.OK).body(customerService.getAll())
+    }
+
+    @GetMapping("/{id}")
+    fun getById(@PathVariable id: Long): ResponseEntity<Customer>{
+        return ResponseEntity.status(HttpStatus.OK).body(customerService.getById(id))
+    }
 }
--- src/main/kotlin/br/com/lucasladeira/mercadolivro/services/CustomerService.kt
+++ src/main/kotlin/br/com/lucasladeira/mercadolivro/services/CustomerService.kt
@@ -3,11 +3,25 @@ package br.com.lucasladeira.mercadolivro.services
 import br.com.lucasladeira.mercadolivro.entities.Customer
 import br.com.lucasladeira.mercadolivro.repositories.CustomerRepository
 import org.springframework.stereotype.Service
+import java.util.Optional
+import javax.persistence.EntityNotFoundException
 
 @Service
 class CustomerService(var customerRepository: CustomerRepository) {
 
     fun save(customer: Customer): Customer{
         return customerRepository.save(customer)
+    }
+
+    fun getAll(): List<Customer>{
+        return customerRepository.findAll()
+    }
+
+    fun getById(id: Long): Customer{
+        val opt: Optional<Customer> = customerRepository.findById(id)
+        if (opt.isEmpty){
+            throw EntityNotFoundException("Customer not found!")
+        }
+        return opt.get()
     }
 }
