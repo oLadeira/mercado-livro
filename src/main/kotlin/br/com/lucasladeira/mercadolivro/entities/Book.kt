b4ca2e9439c5d8319b735c6271e770b65ddedaed
Lucas Ladeira <lucas-ladeira@hotmail.com>
2022-06-23 14:43:57 -0300

    bloqueando alteracao de status de livros cancelados/deletados

--- src/main/kotlin/br/com/lucasladeira/mercadolivro/entities/Book.kt
+++ src/main/kotlin/br/com/lucasladeira/mercadolivro/entities/Book.kt
@@ -12,9 +12,27 @@ data class Book(
     var id: Long? = null,
     var name: String? = null,
     var price: BigDecimal? = null,
-    @Enumerated(EnumType.STRING)
-    var status: BookStatus? = BookStatus.ATIVO,
+
     @ManyToOne
     @JoinColumn(name = "customer_id")
     var customer: Customer? = null
-)
+){
+    @Enumerated(EnumType.STRING)
+    var status: BookStatus? = BookStatus.ATIVO
+    set(value){
+        if (value==BookStatus.DELETADO || value==BookStatus.CANCELADO){
+            throw Exception("Não é possível deletar um livro com o status ${field}")
+        }
+        field = value
+    }
+
+    constructor(
+        id: Long? = null,
+        name: String? = null,
+        price: BigDecimal? = null,
+        customer: Customer? = null,
+        status: BookStatus? = BookStatus.ATIVO
+    ): this(id, name, price, customer){
+        this.status = status
+    }
+}
